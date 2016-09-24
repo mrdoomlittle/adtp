d9323790d444109b7c5226d5e9ce187dba5d8c90
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2016-09-24 23:58:04 +0100

    added code from another project of mine (carg_filter), it currently does not work but will get it working the next day

---
+++ carg_filter.cpp
@@ -0,0 +1,103 @@
+/*this code is from another one of my projects i will get this working the next day*/
+
+char const (* * __arg__filter::filter_list_in (char const (* _char_list_in ),
+   char const (* _separator_char ), size_t (_char_list_length ) ) )
+{
+   if (_char_list_in != nullptr && (this-> char_list_in ) == nullptr )
+        (this-> char_list_in ) = _char_list_in;
+
+   if (_separator_char != nullptr && (this-> separator_char ) == nullptr )
+      (this-> separator_char ) = _separator_char;
+
+   if (_char_list_length > 1 && (this-> char_list_length ) < 1 )
+      (this-> char_list_length ) = _char_list_length;
+
+   if ((this-> char_list_in ) == nullptr || (this-> separator_char ) == nullptr) return (nullptr );
+
+   if ( (this-> char_list_length ) < 1 ) return (nullptr );
+
+   (this-> separator_mapping ) = new bool [(this-> char_list_length ) ];
+
+   for (size_t (list_pos ) = 0; list_pos != (this-> char_list_length); list_pos ++)
+   {
+      if ((this-> char_list_in [list_pos ]) == (this-> separator_char [0]) )
+         (this-> separator_mapping [list_pos]) = true;
+
+      if ((this-> char_list_in [list_pos ]) != (this-> separator_char [0]) )
+         (this-> separator_mapping [list_pos]) = false;
+   }
+
+   for (size_t (list_pos ) = 0; list_pos != nums_add <unsigned int> ( (this-> char_list_length ), 1); list_pos ++)
+   {
+      if (list_pos == (this-> char_list_length ) )
+      {
+         if ( (this-> is_sub_counted ) == true && (this-> got_sub_size ) == true )
+         {
+
+            for (size_t (sub_list_pos ) = 0; sub_list_pos != (this-> sub_counter ); sub_list_pos ++)
+               for (size_t (list_pos ) = 0; list_pos != (this-> sub_list_size [sub_list_pos ] ); list_pos ++)
+                  (this-> temp_out_list [sub_list_pos ] [list_pos ] ) = (this-> char_list_in [ (nums_add <unsigned int> (list_pos, (this-> sub_start_point [sub_list_pos ] ) ) ) ] );
+
+            break;
+         }
+
+         if ( (this-> is_sub_counted ) == true && (this-> got_sub_size ) == false )
+         {
+            (this-> got_sub_size ) = true;
+            for (size_t (sub_list_pos ) = 0; sub_list_pos != (this-> sub_counter ); sub_list_pos ++)
+               (this-> temp_out_list [sub_list_pos ] ) = new char [(this-> sub_list_size [sub_list_pos ] ) ];
+         }
+
+         if ( (this-> is_sub_counted ) == false )
+         {
+            (this-> is_sub_counted ) = true;
+            (this-> sub_list_size ) = new size_t [(this-> sub_counter ) ];
+            (this-> sub_start_point ) = new size_t [(this-> sub_counter ) ];
+            (this-> temp_out_list ) = new char * [(this-> sub_counter ) ];
+         }
+
+         list_pos = 0;
+      }
+
+      if ( (this-> separator_mapping [list_pos ] ) == false && list_pos == 0 )
+      {
+         if ( (this-> is_sub_counted ) == false ) (this-> sub_counter ) ++;
+
+         if ( (this-> is_sub_counted ) == true && (this-> got_sub_size ) == false )
+         {
+            (this-> sub_start_point [(this-> sub_point_change ) ] ) = list_pos;
+            for (size_t (sub_list_pos ) = list_pos; sub_list_pos != (this-> char_list_length ); sub_list_pos ++)
+            {
+               if ((this-> separator_mapping [sub_list_pos ] ) == false ) (this-> sub_list_size [(this-> sub_point_change) ] ) ++;
+               else break;
+            }
+
+            (this-> sub_point_change ) ++;
+         }
+      }
+
+      if ( (this-> separator_mapping [list_pos ] ) == true )
+      {
+         if ( (this-> is_sub_counted ) == false ) (this-> sub_counter ) ++;
+
+         if ( (this-> is_sub_counted ) == true && (this-> got_sub_size ) == false )
+         {
+            (this-> sub_start_point [(this-> sub_point_change ) ] ) = nums_add <int> (list_pos, 1);
+            for (size_t (sub_list_pos ) = nums_add <int> (list_pos, 1); sub_list_pos != (this-> char_list_length ); sub_list_pos ++)
+            {
+               if ((this-> separator_mapping [sub_list_pos ] ) == false ) (this-> sub_list_size [(this-> sub_point_change) ] ) ++;
+               else break;
+            }
+
+            (this-> sub_point_change ) ++;
+         }
+      }
+   }
+
+   (this-> char_list_out ) = const_cast <const char * *> (this-> temp_out_list );
+
+   return (this-> char_list_out );
+}
+
+size_t (__arg__filter::get_sub_counter( ) ) { return (this-> sub_counter ); }
+size_t (* __arg__filter::get_sub_list_size( ) ) { return (this-> sub_list_size ); }
---
+++ carg_filter.hpp
@@ -0,0 +1,52 @@
+# ifndef __arg__filter__hpp__
+# define __arg__filter__hpp__
+/*this code is from another one of my projects i will get this working the next day*/
+# include <cstdlib>
+
+class __arg__filter
+{
+   private :
+      char const (* char_list_in ) = nullptr;
+      const char (* * char_list_out ) = nullptr;
+      char const (* separator_char ) = nullptr;
+
+      size_t (char_list_length ) = 0;
+
+      // we way to check where the separator is located
+      // example : ["Hello,All"] = [0,0,0,0,0,1,0,0,0]
+      bool (* separator_mapping ) = nullptr;
+
+      size_t (sub_counter ) = 0;
+
+      size_t (* sub_list_size ) = nullptr;
+
+      bool (is_sub_counted ) = false;
+      bool (got_sub_size ) = false;
+
+      size_t (* sub_start_point ) = nullptr;
+
+      size_t (sub_point_change ) = 0;
+
+      char (* * temp_out_list ) = nullptr;
+
+    public :
+      char const (* * filter_list_in (char const (* _char_list_in ) = nullptr,
+      char const (* _separator_char ) = nullptr, size_t (_char_list_length ) = 0) ) ;
+
+      void (set_list_length (size_t (_char_list_length ) = 0 ) );
+      size_t (get_list_length( ) );
+
+      void (set_list_in (char const (* _char_list_in ) = nullptr) );
+      char const (* get_list_in( ) );
+
+      void (set_list_out (char const (* * _char_list_out ) = nullptr) );
+      char const (* * get_list_out( ) );
+
+      void (set_separator (char const (* separator_char ) = nullptr) );
+      char const (* get_separator( ) );
+
+      size_t (get_sub_counter( ) );
+      size_t (* get_sub_list_size( ) );
+} ;
+
+# endif /*__arg__filter__hpp__*/
